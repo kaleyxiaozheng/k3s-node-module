d8927777365ba0bd03276bc837879317835e30df
Kaley Zheng <zheng.kaley.x@gmail.com>
2026-06-20 22:55:56 +1000

    update main

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # modules/k3s-node/main.tf
 resource "proxmox_virtual_environment_file" "config" {
   content_type = "snippets"
-  datastore_id = "local"
+  datastore_id = "local" # 确保此 datastore 已启用 Snippets 功能
   node_name    = "pve"
 
   source_raw {
@@ -14,9 +14,7 @@ resource "proxmox_virtual_environment_vm" "node" {
   node_name = "pve"
   vm_id     = var.vm_id
 
-  clone { 
-    vm_id = 100 
-  } # use Ubuntu template with cloud-init support (assumes template ID is 100)
+  clone { vm_id = 100 } # use Ubuntu template with cloud-init support (assumes template ID is 100)
 
   cpu { 
     cores = var.cpu_cores
@@ -31,24 +29,33 @@ resource "proxmox_virtual_environment_vm" "node" {
   }
 
   initialization {
-    datastore_id      = "local-lvm"
+    datastore_id = "local-lvm"
     user_data_file_id = proxmox_virtual_environment_file.config.id
     
     # dynamically check if it is using static IP
-    dynamic "ip_config" {
-      for_each = var.static_ip != null ? [1] : []
-      content {
-        ipv4 { 
-          address = var.static_ip
-          gateway = var.gateway 
-        }
+    ip_config {
+      ipv4 {
+        address = var.static_ip != null ? var.static_ip : "dhcp"
+        gateway = var.gateway
       }
     }
   }
 lifecycle {
     ignore_changes = [
       initialization[0].ip_config,
-      network_device
     ]
+  }
+}
+
+resource "null_resource" "wait_for_node" {
+  depends_on = [proxmox_virtual_environment_vm.node]
+  
+  provisioner "local-exec" {
+    # 如果静态 IP 已定义，则使用它；否则需要根据具体情况通过其他方式检查
+    command = <<EOT
+      echo "Waiting for node ${var.node_name} to be reachable..."
+      # 这里可以添加简单的超时逻辑来确认 VM 已完成 Cloud-Init 的网络配置
+      sleep 30
+    EOT
   }
 }
