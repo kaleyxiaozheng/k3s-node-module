01fadbf445a928660d79257281965ff465ecc695
Kaley Zheng <zheng.kaley.x@gmail.com>
2026-06-21 21:10:12 +1000

    update local-lvm

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "proxmox_virtual_environment_vm" "node" {
   }
 
   initialization {
-    datastore_id      = "local" # Must use same datastore as above file resource
+    datastore_id      = "local-lvm" # Must use same datastore as above file resource
     user_data_file_id = proxmox_virtual_environment_file.config.id
     
     # dynamically check if it is using static IP
